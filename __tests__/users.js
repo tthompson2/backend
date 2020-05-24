9d080555fa9d7996897a8226a4441e373f0532db
Trevor Thompson <Thompson_Trevor@bah.com>
2020-05-24 19:28:23 -0400

    Updating testing code to include new endpoint tests

--- __tests__/users.js
+++ __tests__/users.js
@@ -1,7 +1,6 @@
 const supertest = require("supertest");
 const server = require("../index");
 const db = require("../database/dbConfig");
-const user = require("../auth/auth-router");
 
 beforeEach(async () => {
     await db.seed.run()
@@ -12,12 +11,12 @@ afterAll(async () => {
 })
 
 describe("user endpoint unit tests", () => {
-    it("Get /register ", async() => {
+    it("Get /register ", async () => {
         const res = await supertest(server).get("/api/auth/register")
         expect(res.statusCode).toBe(400);
     })
 
-    it("Get /login", async() => {
+    it("Get /login", async () => {
         const res = await supertest(server).get("/api/auth/login")
         expect(res.statusCode).toBe(400);
     })
@@ -26,11 +25,11 @@ describe("user endpoint unit tests", () => {
 describe("registration endpoint testing", () => {
 
     beforeEach(async () => {
-        await db("users").truncate()
+        await db("user").truncate()
     })
 
-    it("POST register add new user", async() => {
-        const data = {username: "trev108", password: "8888888"}
+    it("POST register add new user", async () => {
+        const data = { username: "trev108", password: "8888888" }
         const res = await supertest(server).post("/api/auth/register").send(data)
         expect(res.status).toBe(201);
     })
@@ -40,23 +39,23 @@ describe("registration endpoint testing", () => {
 describe("login endpoint testing", () => {
 
     beforeEach(async () => {
-        await db("users").truncate();
+        await db("user").truncate();
     })
 
     it("POST /login failure", async () => {
-            const data = {username: "trev51", password: "123456"}
-            const res = await supertest(server).post("/api/auth/login").send(data);
-            expect(res.statusCode).toBe(401);
+        const data = { username: "trev51", password: "123456" }
+        const res = await supertest(server).post("/api/auth/login").send(data);
+        expect(res.statusCode).toBe(401);
     })
 
     it("POST successful login", async () => {
         try {
-            const data = {username: "trev4", password: "4155478713"}
+            const data = { username: "trev4", password: "4155478713" }
             await supertest(server).post("/api/auth/register").send(data)
             const res = await (await supertest(server).post("/api/auth/login")).send(data);
             expect(res.statusCode).toBe(200)
-        } catch(err) {
-            throw(err)
+        } catch (err) {
+            throw (err)
         }
     })
 })
