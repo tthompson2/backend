726cb4a6f249460f3f184938925ce52a55f4e08d
Trevor Thompson <Thompson_Trevor@bah.com>
2020-06-01 22:14:51 -0400

    Adding server.listen back to configuration

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require('./api/server.js');
 
-// const PORT = process.env.PORT || 4000;
-// server.listen(PORT, () => {
-//   console.log(`\n=== Server listening on port ${PORT} ===\n`);
-// });
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'running' });
